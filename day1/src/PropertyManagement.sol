33f52f011cfd3f5469dc959fbda153c19a9dabc8
halima <dahunsiolajumoke18@gmail.com>
2026-02-23 20:30:53 +0100

    Task updated

--- day1/src/PropertyManagement.sol
+++ day1/src/PropertyManagement.sol
@@ -1,49 +1,73 @@
   // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
-// import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-// import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+
 contract PropertyManagement {
     // using SafeERC20 for IERC20;
 
-    IERC20 public paymentToken;
+    IERC20 public immutable paymentToken;
+    address tokenAddress ;
+
+    error NOT_THE_OWNER();
+
+    constructor (address _tokenAddress) {
+        tokenAddress =_tokenAddress;
+
+        paymentToken = IERC20 (_tokenAddress);
+    }
 
    struct  Properties {
     uint id;
     string apartmentType;
+    string description;
     uint amount;
     address OwnerAddress;
+    uint timeStamp;
+    
    }
 
    mapping (uint  => uint) propertyIdToIndex;
    mapping (uint => Properties) propertyDetails;
    address owner = msg.sender;
+
    modifier  OnlyOwner (){
-    require(msg.sender == owner, "This action has been restricted to only owner");
-    _;
+if (msg.sender != owner) {
+    revert NOT_THE_OWNER();
+}    _;
    }
+
    Properties [] public properties;
    uint propertyId;
-   function addProperty (string memory _apartmentType, uint _amount ) external returns (bool){
+   function buyProperty (string memory _apartmentType, uint _amount, string memory _description ) external returns (bool){
     propertyId =propertyId + 1;
     Properties memory property = Properties ({
         id: propertyId,
         apartmentType : _apartmentType,
+        description: _description,
         amount : _amount,
-        OwnerAddress: msg.sender
+        OwnerAddress: msg.sender,
+        timeStamp : block.timestamp
     });
     properties.push(property);
 
     return true;
    }
 
-   function makePayment(uint _id) external {
+   function buyProperty(uint _id) external {
     require(_id < properties.length, "Property not found");
-    index = propertyIdToIndex[_id];
-    selectedProp = properties[index] ;
-    uint amountToPay = selectedProp.amount;
+   uint indexx = propertyIdToIndex[_id];
+for (uint i; i < properties.length; i++){
+    if(properties[i].id == _id){
+    uint amountToPay = properties[i].amount;
 
-    bool success = Transfer(msg.sender, address(this), amountToPay);
+    bool success = paymentToken.TransferFrom(msg.sender, address(this), amountToPay);
     require (success, "Transaction failed, please try again");
+
+    }
+}
+
 
    }
    function removeProperty(uint _id) external OnlyOwner {
@@ -62,11 +86,11 @@ contract PropertyManagement {
 
    }
 
-   function getAllProperties() external returns (Properties [] memory){
+   function getAllProperties() external view returns (Properties [] memory){
     return properties;
    }
 
-   function getPropertyById(uint _id) external returns (Properties memory){
+   function getPropertyById(uint _id) external view returns (Properties memory){
     require(_id < properties.length, "Property not found");
     return properties[_id];
    }
